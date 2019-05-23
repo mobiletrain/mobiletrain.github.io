7e17d9ada7af2337de5f8111d9173b87873e0a34
小明 <wumingfu@1000phone.com>
2019-05-23 11:08:01 +0800

    init

---
+++ js/config.js
@@ -0,0 +1,1 @@
+"use strict";require.config({baseUrl:"/",paths:{jquery:"libs/jquery/jquery-1.12.4.min",loadHF:"js/loadHeaderAndFooter",swiper:"libs/swiper/js/swiper.min",template:"libs/art-template/template-web",fly:"libs/jquery-plugins/jquery.fly.min",zoom:"libs/jquery-plugins/jquery.elevateZoom-3.0.8.min"},shim:{fly:{deps:["jquery"]},zoom:{deps:["jquery"]}}});
